0882c55ca0fe43ef4a6717960a5792af83e74cc5
Simon Marache <git@simon.marache.net>
2023-02-05 19:12:33 +0000

    More realistic benchmark (larger image, no sin/cos)

--- benches/line_draw.rs
+++ benches/line_draw.rs
@@ -1,27 +1,24 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use std::{f32::consts::PI};
-
 use mytinyrenderer::line_draw::draw_line;
 use mytinyrenderer::image::{Image, Pixel};
 use mytinyrenderer::point::Point;
 
 fn line_draw_all_inside(image: &mut Image) {
-    for d in 0..100 {
-        let color = Pixel::new(2 * d as u8, 255 - 2 * d as u8, 255 - d as u8);
-        let d = ((d as f32) / 100.) * (2. * PI);
-
+    for d in 0..200 {
+        let color = Pixel::new(d as u8, 255 - d as u8, d % 64 + 128 as u8);
+        let df = d as f32;
         draw_line(
             image,
-            Point::new(320., 240.),
-            Point::new(300. * d.cos() + 320., 200. * d.sin() + 240.),
+            Point::new(100. + 5. * df, 100. + 4. * df),
+            Point::new(1900. - 3. * df, 200. + 6. * df),
             color,
         );
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut image = Image::new(640, 480);
+    let mut image = Image::new(1920, 1080);
     c.bench_function("line_draw_all_inside", |b| b.iter(|| line_draw_all_inside(black_box(&mut image))));
 }
 
